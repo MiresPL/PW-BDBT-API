a88442c79b2795a7b651bc30c10f210a80743be4
MiresPL <mateusz.skorek03@gmail.com>
2024-12-13 02:13:10 +0100

    pierwsze próby i testy Thymeleaf'a

--- src/main/java/com/mires/bdbt/parkrozrywki/controllers/TestController.java
+++ src/main/java/com/mires/bdbt/parkrozrywki/controllers/TestController.java
@@ -1,16 +1,18 @@
 package com.mires.bdbt.parkrozrywki.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/")
 @CrossOrigin(value = "*", origins = "*")
 public class TestController {
-    @GetMapping("/test")
-    public String getTest() {
-        return "Test";
+    @GetMapping("")
+    public String home(final Model model) {
+        model.addAttribute("message", "Hello World!");
+        return "home/Home";
     }
 }
