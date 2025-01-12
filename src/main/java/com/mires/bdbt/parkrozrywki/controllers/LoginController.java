734dbb46b4259f3c8b0683badf9870f7526b41c4
MiresPL <mateusz.skorek03@gmail.com>
2025-01-12 18:44:48 +0100

    fix

--- src/main/java/com/mires/bdbt/parkrozrywki/controllers/LoginController.java
+++ src/main/java/com/mires/bdbt/parkrozrywki/controllers/LoginController.java
@@ -1,10 +1,10 @@
 package com.mires.bdbt.parkrozrywki.controllers;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mires.bdbt.parkrozrywki.entities.Klient;
 import com.mires.bdbt.parkrozrywki.services.KlientService;
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,17 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/klient/login")
 public class LoginController {
     private final KlientService klientService;
-    private final Gson compactGson;
 
     public LoginController(KlientService klientService) {
         this.klientService = klientService;
-        this.compactGson = new GsonBuilder()
-                .setPrettyPrinting()
-                .serializeNulls()
-                .setVersion(1.0)
-                .disableHtmlEscaping()
-                .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
-                .create();
     }
 
 
@@ -32,9 +24,13 @@ public class LoginController {
     @PostMapping
     public String login(@RequestParam String login, @RequestParam String password) {
         final Klient klient = klientService.login(login, password);
-        final JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("result", klient != null);
-        jsonObject.addProperty("klient", (klient != null ? compactGson.toJson(klient) : ""));
+        final JSONObject jsonObject = new JSONObject();
+        jsonObject.put("result", klient != null);
+        try {
+            jsonObject.put("klient", (klient != null ? new ObjectMapper().writeValueAsString(klient) : ""));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         return jsonObject.toString();
     }
 }
